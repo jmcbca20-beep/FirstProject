1bb898e69f49d36db1fcb2404670f7c66c7e905e
jmcbca20-beep <jmcbca20@gmail.com>
2025-12-29 10:30:23 +0545

    Update Task1.java
    price change

--- src/Task1.java
+++ src/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
 		// TODO Auto-generated method stub
 		Car maruti=new Car();
 		maruti.Color="Red";
-		maruti.Price=12345;
+		maruti.Price=123456;
 		System.out.println("The Color of car is "+ maruti.Color + " Price is "+ maruti.Price);
 	}
 
